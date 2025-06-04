ca4fa5c09abe378a34d08a6bdf9b3b497b8c0b13
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2025-06-04 11:03:04 +0200

    Initial commit

---
+++ src/main/java/com/banking/home/spring/SpringHomeBankingApplication.java
@@ -0,0 +1,13 @@
+package com.banking.home.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomeBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/home/spring/SpringHomeBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.home.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
